bd87ef83e2bec8158d43bee7acf6aeb365dcd55c
asce <asce@asce-SAM.(none)>
2013-06-16 21:42:47 +0200

    Concurrent server with threads

---
+++ threads/cli_eje.c
@@ -0,0 +1,208 @@
+#include <stdio.h>
+#include <malloc.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/time.h>
+#include <stdlib.h>
+#include "userMSN.h"
+
+
+void main(int argc, char* argv[])
+{
+  int sd,EOT=0,result;
+   struct sockaddr_in sockname;
+   request_msn_t req;
+   char* ip_server;
+   char buffer[82];
+   char secret[256];
+   int challenge,i;
+   unsigned char out[16];
+   if(argc > 2){
+     printf("Numero de argumentos incorrecto:\n\
+Se esperaba %s [ip_servidor]",argv[0]);
+     exit(1);
+   }else if(argc == 1){
+     ip_server = "127.0.0.1";
+   }else if(argc == 2){
+     ip_server = argv[1];
+   }
+    if((sd=socket(AF_INET,SOCK_STREAM,0))==-1)
+        perror("Cliente:Socket"),exit(1);
+    sockname.sin_family=AF_INET;
+    sockname.sin_addr.s_addr=inet_addr(ip_server);
+                                    /*  ^^^^^^^^^^^^   Direccion server  */
+    sockname.sin_port=htons(5001);
+                       /*   ^^^^  Sustituir por el puerto correspondiente */
+    if(connect(sd,(struct sockaddr*) &sockname,sizeof(sockname))==-1){
+      perror("Cliente:Connect");
+      exit(1);
+    }
+    
+    do{
+      char user_name[256];
+      char location[25];
+      unsigned char out[16];
+      int state;
+      puts("Seleccione accion:");      
+      showMenu();
+      int opt;
+      scanf("%i",&opt);
+      switch(opt){
+      case GET_STATE:
+	scanf("%s",user_name);
+	sprintf(buffer,"%i %s",GET_STATE,user_name);
+	if(send(sd,buffer,80,0)==-1){
+	  perror("Cliente:Send");
+	  exit(1);
+	}
+	//Esperar respuesta.
+	//printf("Espera respuesta\n");
+	result = recv(sd,buffer,80,0);
+	//printf("%s\n",buffer);
+	if(result==-1){
+          perror("Cliente:Recv");
+          exit(1);
+        }
+	split_request(buffer,&req);
+	if(req.req_type==STATE)
+	  printf("%s STATE %s\n",req.req[0],state2str(atoi(req.req[1])));
+	else if(req.req_type==ERROR)
+	  printf("No existe el usuario %s.\n",user_name);
+	//
+	break;
+      case POST_NAME_LOCATION_STATE:
+	//printf("Introduce nombre\n")
+        scanf("%s",user_name);
+	scanf("%s",location);
+	scanf("%i",&state);
+	scanf("%s",secret);
+	//Primero nos autenticamos
+        sprintf(buffer,"%i %s",AUTH,user_name);
+	if(send(sd,buffer,80,0)==-1){
+          perror("Cliente:Send");
+          exit(1);
+        }
+	result = recv(sd,buffer,80,0);
+	if(result==-1){
+          perror("Cliente:Recv");
+          exit(1);
+        }
+	split_request(buffer,&req);
+	if(req.req_type==CHALLENGE){
+	  printf("CHALL (RETO) Recibido %i\n",atoi(req.req[0]));
+	//generar md5 user_name:reto:secreto
+	  sprintf(buffer,"%s%i%s",user_name,atoi(req.req[0]),secret);
+	  printf("MD5 %s : ",buffer);
+	  getMD5(buffer,strlen(buffer),out);
+	  for (i=0; i<16; i++) {
+	    printf("%02x", out[i]);
+	  }
+	  printf("\n");
+	  //Enviamos datos update
+	  sprintf(buffer,"%i %s %s %i",POST_NAME_LOCATION_STATE,
+		  user_name,location,state);	
+
+	  if(send(sd,buffer,80,0)==-1){
+	    perror("Cliente:Send");
+	    exit(1);
+	  }	  
+	  //Enviamos md5
+	  if(send(sd,out,16,0)==-1){
+	    perror("Cliente:Send");
+	    exit(1);
+	  }
+	  result = recv(sd,buffer,80,0);
+	  if(result==-1){
+	    perror("Cliente:Recv");
+	    exit(1);
+	  }
+	  split_request(buffer,&req);
+	  if(req.req_type==OK){
+	    printf("Datos de usuario actualizados.\n");
+	  }else	if(req.req_type==ERROR){
+	    printf("El codigo MD5 no es correcto.\nDatos no actualizados\n");
+	    
+	  }
+
+	  
+	  //Esperamos respuesta
+	}else if(req.req_type==ERROR){
+
+	  printf("El usuario %s no existe.\n",user_name);
+
+	}
+	//Hemos sido autenticados
+	/*
+        sprintf(buffer,"%i %s %s %i",POST_NAME_LOCATION_STATE,
+		user_name,location,state);	
+
+	getMD5(buffer,strlen(buffer),out);
+	printf("%s\n",out);
+        if(send(sd,buffer,80,0)==-1){
+          perror("Cliente:Send");
+          exit(1);
+        }
+	result = recv(sd,buffer,80,0);
+	if(result==-1){
+          perror("Cliente:Recv");
+          exit(1);
+        }
+	split_request(buffer,&req);
+	if(req.req_type==OK)
+	printf("OK Recibido\n");*/
+	break;
+      case GET_LOCATION:
+        scanf("%s",user_name);
+        sprintf(buffer,"%i %s",GET_LOCATION,user_name);
+        if(send(sd,buffer,80,0)==-1){
+          perror("Cliente:Send");
+          exit(1);
+        }
+	
+	//Esperar respuesta.
+	printf("Espera respuesta\n");
+	result = recv(sd,buffer,80,0);
+	printf("%s\n",buffer);
+	if(result==-1){
+          perror("Cliente:Recv");
+          exit(1);
+        }
+	split_request(buffer,&req);
+	if(req.req_type==LOCATION)
+	  printf("%s LOCATION %s\n",req.req[0],req.req[1]);
+	else if(req.req_type==ERROR)
+	  printf("No existe el usuario %s.\n",user_name);
+	//
+	break;
+      case EXIT:
+	sprintf(buffer, "%i", EXIT);
+	if(send(sd,buffer,80,0)==-1){
+          perror("Cliente:Send");
+          exit(1);
+	}
+	shutdown(sd,1);//Dejar de 1-transmitir
+	EOT=1;
+	puts("Fin de la transmision.");
+      }
+      /*
+      if(gets(buffer)==NULL){
+	if(send(sd,"EOT",80,0)==-1){
+          perror("Cliente:Send");
+          exit(1);
+	}
+	shutdown(sd,1);//Dejar de 1-transmitir
+	EOT=1;
+	puts("Fin de la transmision.");
+      }
+      
+      if(!EOT){
+	if(send(sd,buffer,80,0)==-1){
+	  perror("Cliente:Send");
+	  exit(1);
+	}
+      }*/  
+    }while(!EOT);
+}
---
+++ threads/userMSN.h
@@ -0,0 +1,188 @@
+#include <string.h>
+#include <stdlib.h>
+#define MAX_NUM_PARAMS_REQUEST 10
+
+typedef enum{
+  AUSENTE,
+  PRESENTE,
+}state_t;
+
+typedef enum{
+  GET_STATE,
+  POST_NAME_LOCATION_STATE,
+  GET_LOCATION,
+  AUTH,
+  CHALLENGE,
+  EXIT,
+}request_type_t;
+
+typedef enum{
+  OK,
+  ERROR,
+  STATE,
+  LOCATION,
+}response_t;
+
+
+
+typedef struct{
+  state_t state;
+  char name[50];
+  char location[15];
+  char secret[256];
+}user_t;
+
+struct node_type{
+  user_t user;
+  struct node_type* next;
+  struct node_type* previous;
+};// node_t;
+typedef struct node_type node_t;
+
+typedef struct{
+  char* req[MAX_NUM_PARAMS_REQUEST+1];
+  int num_params;
+  request_type_t req_type;
+}request_msn_t;
+
+
+
+int split_request(char* req_buff, request_msn_t* req);
+void usercpy(user_t *dest,user_t* src);
+void freeRequest(request_msn_t* req);
+node_t* addUser(user_t* user,node_t* user_list);//SEGFAULT en usercpy
+void showUser(user_t* user);
+char* state2str();
+
+int split_request(char* req_buff, request_msn_t* req){
+  
+  char* str =req_buff;
+  char * pch;
+  char *saveptr;
+  int index = 0;
+  req->num_params = 0;
+  //printf ("Splitting string \"%s\" into tokens:\n",str);
+  pch = strtok_r (str," ,-",&saveptr);
+  if(pch == NULL)
+    return 0;
+  req->req_type = atoi(pch);
+  pch = strtok_r (NULL," ,-",&saveptr);
+
+  
+  while (pch != NULL)
+    {
+      req->req[index] = strdup(pch);
+      req->num_params++;
+      index++;
+      //printf ("%s\n",pch);
+      pch = strtok_r (NULL, " ,-",&saveptr);
+    }
+
+  return 1;
+
+}
+
+void freeRequest(request_msn_t* req){
+  int i;
+  for(i = 0; i < req->num_params; i++){
+    free(req->req[i]);
+  }
+}
+
+node_t* addUser(user_t* user,node_t* user_list){
+
+  node_t* actual_node = user_list;
+  if(actual_node==NULL){
+    actual_node = malloc(sizeof(node_t));
+    usercpy(&actual_node->user,user);
+    actual_node->next=NULL;
+    actual_node->previous = NULL;
+    return actual_node;
+
+  }
+  for(actual_node = user_list; actual_node!= NULL ;
+      actual_node = actual_node->next ){
+    if(strcmp(actual_node->user.name,user->name)==0){
+      usercpy(&actual_node->user,user);
+      return user_list;
+    }
+    if(actual_node->next == NULL){
+      actual_node->next = malloc(sizeof(node_t));
+      usercpy(&actual_node->next->user,user); 
+      actual_node->next->next=NULL;
+      actual_node->next->previous = actual_node->next;
+      return user_list;
+    }
+  }
+  
+}
+
+void usercpy(user_t *dest,user_t* src){
+
+  dest->state = src->state;
+  strcpy(dest->name,src->name);
+  strcpy(dest->location,src->location);
+  strcpy(dest->secret,src->secret);
+
+}
+
+void showUser(user_t* user){
+  printf("Name: %s\nLocation: %s\nState: %s\n\n",user->name,
+	 user->location,state2str(user->state));
+}
+
+char* state2str(state_t state){
+
+  if(state==AUSENTE)
+    return "ausente";
+  else return "presente";
+
+}
+
+node_t* removeUser(char* user_name,node_t* list){
+  node_t* actual = list;
+  node_t* tmp ;
+  while(actual!=NULL){
+    if(strcmp(actual->user.name,user_name)==0){
+      actual->previous->next=actual->next;
+      tmp = actual->next;
+      free(actual);
+      actual = tmp;
+    }
+    actual=actual->next;
+  }
+  return NULL;
+}
+user_t* getUser(char* user_name,node_t* list){
+  node_t* actual = list;
+
+  while(actual!=NULL){
+    if(strcmp(actual->user.name,user_name)==0){
+      return &actual->user;
+    }
+    actual = actual->next;
+  }
+  return NULL;
+}
+
+
+void showUsers(node_t* list){
+  node_t* actual = list;
+  while(actual!=NULL){
+    showUser(&actual->user);
+    actual=actual->next;
+
+  }
+
+}
+
+
+
+void showMenu(){
+
+  printf("%i - Get state from 'user'\n",GET_STATE);
+  printf("%i - Post name,location and state - user_secret\n",POST_NAME_LOCATION_STATE);
+  printf("%i - Get location 'user'\n",GET_LOCATION);
+  printf("%i - Close conection\n\n",EXIT);
+
+}
